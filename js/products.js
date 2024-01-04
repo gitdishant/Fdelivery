009706af5c75209a34d322cbe2ca4ff90afdaf88
dishant <randomv637@gmail.com>
2024-01-05 00:43:54 +0530

    second commit

--- js/products.js
+++ js/products.js
@@ -1,25 +1,25 @@
 /* ========== Products Slider =========== */
 const swiper = new Swiper(".mySwiper", {
-    grabCursor: true,
-    slidesPerView: 1,
-    spaceBetween: 30,
-    pagination: {
-      el: ".custom-pagination",
-      clickable: true,
+  grabCursor: true,
+  slidesPerView: 1,
+  spaceBetween: 30,
+  pagination: {
+    el: ".custom-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    567: {
+      slidesPerView: 2,
     },
-    breakpoints: {
-      567: {
-        slidesPerView: 2,
-      },
-      996: {
-        slidesPerView: 3,
-      },
-      1200: {
-        slidesPerView: 4,
-      },
+    996: {
+      slidesPerView: 3,
     },
-  });
-  
+    1200: {
+      slidesPerView: 4,
+    },
+  },
+});
+
   /* ========== Fetch the Products =========== */
   
   const getProducts = async () => {
